bd16e0ed6a706858569ab8c10c16ff040e7bda76
Anas Saradar <m.anas.alshiekh.bakri@gmail.com>
2025-01-28 14:00:34 +0300

    feat(templates): add TemplateEnums class to manage HTML template types

---
+++ src/models/enums/TemplateEnums.py
@@ -0,0 +1,5 @@
+from enum import Enum
+
+class TemplateEnum(Enum):
+    MOEY = "moey_template.html"
+    IMAGINE = "imagine_template.html"
--- src/models/enums/__init__.py
+++ src/models/enums/__init__.py
@@ -1,2 +1,3 @@
 from .ResumeEnums import SeniorityLevel, Degree
 from .ResponseSignalEnum import ResponseSignal
+from .TemplateEnums import TemplateEnum
