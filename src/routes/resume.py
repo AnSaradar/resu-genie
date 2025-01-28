4a96d0f85f30ed1853381629efaa3cdf16c41014
Anas Saradar <m.anas.alshiekh.bakri@gmail.com>
2025-01-28 14:02:47 +0300

    feat(templates): add new resume template 'imagine_template.html'

--- src/routes/resume.py
+++ src/routes/resume.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, APIRouter, Depends , status
 from fastapi.responses import JSONResponse, FileResponse
 from helpers.config import get_settings ,Settings
-from models.enums import ResponseSignal
+from models.enums import ResponseSignal,TemplateEnum
 from models.schemas import Resume
 from controllers import ResumeController
 from jinja2 import Environment, FileSystemLoader
@@ -9,6 +9,8 @@ from weasyprint import HTML
 import pdfkit
 import logging
 import os
+import asyncio
+from pyppeteer import launch
 
 
 resume_router = APIRouter(
@@ -21,18 +23,29 @@ logger = logging.getLogger(__name__)
 # Initialize Jinja2 environment
 env = Environment(loader=FileSystemLoader("resume_templates"))
 
+
 @resume_router.post("/build")
 async def build_resume(resume_data: Resume, app_settings: Settings = Depends(get_settings)):
     try:
 
         resume_data_dict = ResumeController().prepare_resume_data(resume = resume_data)
-        template = env.get_template("template.html")
+        template = env.get_template(TemplateEnum.IMAGINE.value)
         rendered_html = template.render(**resume_data_dict)
+
+        #html_file_path = "outputs/rendered_resume.html"
+        # with open(html_file_path, "w", encoding="utf-8") as html_file:
+        #     html_file.write(rendered_html)
 
         pdf_file_path = "outputs/resume_output.pdf"
         HTML(string=rendered_html).write_pdf(pdf_file_path)
         
-        
+        # browser = await launch()
+        # page = await browser.newPage()
+
+        # await page.setContent(rendered_html)
+        # await page.pdf({'path': pdf_file_path, 'format': 'A4'})
+        # await browser.close()
+
         return FileResponse(
             pdf_file_path,
             media_type="application/pdf",
