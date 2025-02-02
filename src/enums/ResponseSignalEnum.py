957354c8b83f7f567fb5bc87546abcc61ffa59a4
Anas Saradar <m.anas.alshiekh.bakri@gmail.com>
2025-02-02 10:09:43 +0300

    feat(enums): update DatabaseEnums and ResponseSignal enums

--- src/enums/ResponseSignalEnum.py
+++ src/enums/ResponseSignalEnum.py
@@ -5,3 +5,10 @@ class ResponseSignal(Enum):
 
     RESUME_BUILT_SUCCESS = "Resume has been built successfully"
     RESUME_BUILT_FAILURE = "Error while building resume"
+
+    LOGIN_FAILED = "Failed to login"
+    LOGIN_SUCCESS = "Login done successfully"
+    INCORRECT_CREDENTIALS = "Invalid Email or Password"
+
+    USER_CREATION_FAILED = "User creation failed"
+    USER_CREATION_SUCCESS = "User created successfully"
