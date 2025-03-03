b50dc69c084094c323f6fb020c04dbf55c99499a
Anas Saradar <m.anas.alshiekh.bakri@gmail.com>
2025-03-03 17:46:53 +0300

    feat(dto): add Education DTOs for create, update, and response handling

---
+++ src/dto/education.py
@@ -0,0 +1,44 @@
+from pydantic import BaseModel, Field
+from datetime import date
+from typing import Optional
+from enums import Degree
+
+class EducationCreate(BaseModel):
+
+    institution: str = Field(..., description="Name of the educational institution")
+    degree: Degree = Field(..., description="Degree obtained or pursued")
+    field: str = Field(..., description="Field of study (e.g., Information Technology Engineering)")
+    start_date: date = Field(..., description="Start date of the education program")
+    currently_studying: bool = Field(..., description="Indicates if the individual is still pursuing this degree")
+    end_date: Optional[date] = Field(None, description="End date of the education program, if applicable")
+    description: Optional[str] = Field(None, description="Details about the education program")
+
+    class Config:
+            orm_mode = True
+
+class EducationUpdate(BaseModel):
+
+    institution: Optional[str] = Field(None, description="Name of the educational institution")
+    degree: Optional[Degree] = Field(None, description="Degree obtained or pursued")
+    field: Optional[str] = Field(None, description="Field of study (e.g., Information Technology Engineering)")
+    start_date: Optional[date] = Field(None, description="Start date of the education program")
+    currently_studying: Optional[bool] = Field(None, description="Indicates if the individual is still pursuing this degree")
+    end_date: Optional[date] = Field(None, description="End date of the education program, if applicable")
+    description: Optional[str] = Field(None, description="Details about the education program")
+
+    class Config:
+            orm_mode = True
+
+
+class EducationUpdate(BaseModel):
+
+    institution: str = Field(..., description="Name of the educational institution")
+    degree: Degree = Field(..., description="Degree obtained or pursued")
+    field: str = Field(..., description="Field of study (e.g., Information Technology Engineering)")
+    start_date: date = Field(..., description="Start date of the education program")
+    currently_studying: bool = Field(..., description="Indicates if the individual is still pursuing this degree")
+    end_date: Optional[date] = Field(None, description="End date of the education program, if applicable")
+    description: Optional[str] = Field(None, description="Details about the education program")
+
+    class Config:
+            orm_mode = True
