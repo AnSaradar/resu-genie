a4b73e4d859bdbe619bde1f509aa71a603694bdf
Anas Saradar <m.anas.alshiekh.bakri@gmail.com>
2025-04-03 20:33:21 +0300

    chore(main): update main.py to add CORS config and route registration
    Enhanced `main.py` for improved API configuration and routing.
    
    - Added CORS configuration to handle cross-origin requests.
    - Registered all routers, including user, profile, experience, certification, skill, language, link, and resume routes.

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
-from routes import base_router, resume_router, auth_router, user_profile_router, experience_router
+from routes import base_router, resume_router, auth_router, user_profile_router, experience_router, education_router, skill_router, language_router, certification_router, link_router
 from core.config import get_settings, Settings
 from motor.motor_asyncio import AsyncIOMotorClient
 import logging
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
 
@@ -17,6 +19,15 @@ logging.basicConfig(
 )
 
 logger = logging.getLogger(__name__)
+
+# =================CORS Configurations=================
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # This allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # This allows all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # This allows all headers
+)
 
 @app.on_event("startup")
 async def startup():
@@ -36,3 +47,9 @@ app.include_router(resume_router)
 app.include_router(auth_router)
 app.include_router(user_profile_router)
 app.include_router(experience_router)
+app.include_router(education_router)
+app.include_router(skill_router)
+app.include_router(language_router)
+app.include_router(certification_router)
+app.include_router(link_router)
+
