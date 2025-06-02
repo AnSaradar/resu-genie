85e1aa0431d55eeff55452f0db7693cab4a4c354
Lara Faroun <lara.r.faroun@gmail.com>
2025-06-02 13:10:27 +0300

    added llm config vars

--- src/core/config.py
+++ src/core/config.py
@@ -2,6 +2,7 @@ from pydantic_settings import BaseSettings , SettingsConfigDict
 import logging
 import os
 import json
+from typing import Optional
 
 class Settings(BaseSettings):
 
@@ -14,6 +15,25 @@ class Settings(BaseSettings):
     SECRET_KEY : str
     ALGORITHM : str
     ACCESS_TOKEN_EXPIRE_MINUTES : int
+
+    GENERATION_BACKEND :str 
+    EMBEDDING_BACKEND :str 
+
+    OPENAI_API_KEY :str = None
+    OPENAI_API_URL :str = None
+    COHERE_API_KEY :str = None
+
+    GENERATION_MODEL_ID :str = None
+    EMBEDDING_MODEL_ID :str = None
+    EMBEDDING_MODEL_SIZE :int = None
+
+    DEFAULT_INPUT_MAX_CHARACTERS :int = None
+    DEFAULT_GENERATION_MAX_OUTPUT_TOKENS :int = None
+    DEFAULT_GENERATION_TEMPERATURE :float = None
+
+    
+    DEFAULT_LANGUAGE:str ="en"
+    PRIMARY_LANGUAGE:str = "en"
 
     class Config(SettingsConfigDict):
         env_file = '.env'
